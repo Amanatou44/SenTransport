70defffd5cfcb71970d41ea4420d277be4e6092f
Amanatou44 <ekiyastore@gmail.com>
2026-05-22 23:40:12 +0000

    Lab 5 : React connecté à Flask

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -19,6 +19,53 @@ def accueil():
 @app.route("/lignes")
 def get_lignes():
     return jsonify(lignes)
+
+@app.route("/arrets")
+def get_arrets():
+
+    tous_les_arrets = []
+
+    for ligne in lignes:
+        tous_les_arrets.extend(ligne["listeArrets"])
+
+    arrets_uniques = list(set(tous_les_arrets))
+
+    return jsonify(arrets_uniques)
+@app.route("/stats")
+def get_stats():
+
+    total_lignes = len(lignes)
+
+    total_arrets = sum(
+        ligne["arrets"] for ligne in lignes
+    )
+
+    ligne_max = max(
+        lignes,
+        key=lambda ligne: ligne["arrets"]
+    )
+
+    return jsonify({
+        "total_lignes": total_lignes,
+        "total_arrets": total_arrets,
+        "ligne_plus_arrets": ligne_max["numero"]
+    })
+@app.route("/lignes/recherche")
+def rechercher_lignes():
+
+    q = request.args.get("q", "").lower()
+
+    resultats = []
+
+    for ligne in lignes:
+
+        depart = ligne["depart"].lower()
+        arrivee = ligne["arrivee"].lower()
+
+        if q in depart or q in arrivee:
+            resultats.append(ligne)
+
+    return jsonify(resultats)
 
 @app.route("/lignes/<int:ligne_id>")
 def get_ligne(ligne_id):
